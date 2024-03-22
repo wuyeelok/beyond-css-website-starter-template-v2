e6affccc6d26ce68836a16eb8b706b003e93ad9e
wuyeelok <wuyeelok@gmail.com>
2024-03-22 10:45:22 +0800

    Config: Setup for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: resolve(__dirname, "src/"),
+  base: "/beyond-css-website-starter-template-v2/",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
     rollupOptions: {
       input: {
+        404: resolve(__dirname, "src/404.html"),
         home: resolve(__dirname, "src/index.html"),
         about: resolve(__dirname, "src/about/index.html"),
         contact: resolve(__dirname, "src/contact/index.html"),
